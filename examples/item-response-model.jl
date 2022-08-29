2b792d0ad9fb057700aea2eb84883714ca3bd0fd
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2022-08-29 14:36:18 +0100

    initial work on the benchmarking

---
+++ examples/item-response-model.jl
@@ -0,0 +1,100 @@
+using TuringBenchmarking
+using BenchmarkTools
+using Turing
+
+# Trigger Requires.jl.
+using Zygote: Zygote
+using ReverseDiff: ReverseDiff
+
+### Setup ###
+function sim(I, P)
+    yvec = Vector{Int}(undef, I * P)
+    ivec = similar(yvec)
+    pvec = similar(yvec)
+
+    beta = rand(Normal(), I)
+    theta = rand(Normal(), P)
+
+    n = 0
+    for i in 1:I, p in 1:P
+        n += 1
+        ivec[n] = i
+        pvec[n] = p
+        yvec[n] = rand(BernoulliLogit(theta[p] - beta[i]))
+    end
+
+    return yvec, ivec, pvec, theta, beta
+end
+
+P = 1000
+y, i, p, _, _ = sim(20, P);
+
+### Turing ###
+# naive implementation
+@model function irt_naive(y, i, p; I = maximum(i), P = maximum(p))
+    theta ~ filldist(Normal(), P)
+    beta ~ filldist(Normal(), I)
+
+    for n in eachindex(y)
+        y[n] ~ Bernoulli(logistic(theta[p[n]] - beta[i[n]]))
+    end
+end
+
+# performant model
+@model function irt(y, i, p; I = maximum(i), P = maximum(p))
+    theta ~ filldist(Normal(), P)
+    beta ~ filldist(Normal(), I)
+    Turing.@addlogprob! sum(logpdf.(BernoulliLogit.(theta[p] - beta[i]), y))
+
+    return (; theta, beta)
+end
+
+# Instantiate
+model = irt(y, i, p);
+
+# Make the benchmark suite.
+suite = TuringBenchmarking.make_turing_suite(
+    model,
+    adbackends = [TuringBenchmarking.ForwardDiffAD{40}(), TuringBenchmarking.ReverseDiffAD{true}()]
+);
+
+# Run suite!
+@info "Turing.jl" run(suite)
+
+
+### Stan ###
+@info "Compiling Stan model..."
+
+# Tell `TuringBenchmarking` how to convert `model` into data consumable by Stan.
+function TuringBenchmarking.args_to_stan_data(model::DynamicPPL.Model{typeof(irt)})
+    args = Dict(zip(string.(keys(model.args)), values(model.args)))
+    args["N"] = args["I"] * args["P"]
+    return args
+end
+
+# Tell `TuringBenchmarking` about the corresponding Stan model.
+TuringBenchmarking.stan_model_string(model::DynamicPPL.Model{typeof(irt)}) = """
+data {
+    int<lower=1> I;
+    int<lower=1> P;
+    int<lower=1> N;
+    int<lower=1, upper=I> i[N];
+    int<lower=1, upper=P> p[N];
+    int<lower=0, upper=1> y[N];
+}
+parameters {
+    vector[I] beta;
+    vector[P] theta;
+}
+model {
+    theta ~ std_normal();
+    beta ~ std_normal();
+    y ~ bernoulli_logit(theta[p] - beta[i]);
+}
+"""
+
+# Construct benchmark suite.
+stan_suite = TuringBenchmarking.make_stan_suite(model)
+# Run suite!
+@info "Stan" run(stan_suite)
+
---
+++ src/TuringBenchmarking.jl
@@ -0,0 +1,176 @@
+module TuringBenchmarking
+
+using BenchmarkTools
+
+using LogDensityProblems
+using Turing
+using Turing.Essential: ForwardDiffAD, TrackerAD, ReverseDiffAD, ZygoteAD, CHUNKSIZE
+
+using PyCall
+
+const pystan = PyNULL()
+
+function __init__()
+    copy!(pystan, pyimport_conda("pystan", "pystan", "conda-forge"))
+end
+
+# Don't include `TrackerAD` because it's never going to win.
+const DEFAULT_ADBACKENDS = [
+    ForwardDiffAD{40}(),    # chunksize=40
+    ForwardDiffAD{100}(),   # chunksize=100
+    ZygoteAD(),
+    ReverseDiffAD{false}(), # rdcache=false
+    ReverseDiffAD{true}()   # rdcache=false
+]
+
+"""
+    make_turing_suite(model; kwargs...)
+
+Create default benchmark suite for `model`.
+
+# Keyword arguments
+- `adbackends`: a collection of adbackends to use. Defaults to `$(DEFAULT_ADBACKENDS)`.
+- `run_once=true`: if `true`, the body of each benchmark will be run once to avoid
+  compilation to be included in the timings (this may occur if compilation runs
+  longer than the allowed time limit).
+- `save_grads=false`: if `true` and `run_once` is `true`, the gradients from the initial
+  execution will be saved and returned as the second return-value. This is useful if you
+  want to check correctness of the gradients for different backends.
+
+# Notes
+- A separate "parameter" instance (`DynamicPPL.VarInfo`) will be created for _each test_.
+  Hence if you have a particularly large model, you might want to only pass one `adbackend`
+  at the time.
+"""
+function make_turing_suite(
+    model::DynamicPPL.Model;
+    adbackends = DEFAULT_ADBACKENDS, run_once = true, save_grads = false
+)
+    suite = BenchmarkGroup()
+    suite["not_linked"] = BenchmarkGroup()
+    suite["linked"] = BenchmarkGroup()
+
+    grads = Dict(:not_linked => Dict(), :linked => Dict())
+
+    vi_orig = DynamicPPL.VarInfo(model)
+    spl = DynamicPPL.SampleFromPrior()
+
+    for adbackend in adbackends
+        vi = DynamicPPL.VarInfo(vi_orig, spl, vi_orig[spl])
+        f = LogDensityProblems.ADgradient(
+            adbackend,
+            Turing.LogDensityFunction(vi, model, spl, DynamicPPL.DefaultContext())
+        )
+        θ = vi[spl]
+
+        if run_once
+            ℓ, ∇ℓ = LogDensityProblems.logdensity_and_gradient(f, θ)
+
+            if save_grads
+                grads[:not_linked][adbackend] = (ℓ, ∇ℓ)
+            end
+        end
+        suite["not_linked"]["$(adbackend)"] = @benchmarkable $(LogDensityProblems.logdensity_and_gradient)($f, $θ)
+
+        # Need a separate `VarInfo` for the linked version since otherwise we risk the
+        # `vi` from above being mutated.
+        vi_linked = deepcopy(vi)
+        DynamicPPL.link!(vi_linked, spl)
+        f_linked = LogDensityProblems.ADgradient(
+            adbackend,
+            Turing.LogDensityFunction(vi_linked, model, spl, DynamicPPL.DefaultContext())
+        )
+        θ_linked = vi_linked[spl]
+        if run_once
+            ℓ, ∇ℓ = LogDensityProblems.logdensity_and_gradient(f_linked, θ_linked)
+
+            if save_grads
+                grads[:linked][adbackend] = (ℓ, ∇ℓ)
+            end
+        end
+        suite["linked"]["$(adbackend)"] = @benchmarkable $(LogDensityProblems.logdensity_and_gradient)($f_linked, $θ_linked)
+
+    end
+
+    return save_grads ? (suite, grads) : suite
+end
+
+"""
+    args_to_stan_data(model::DynamicPPL.Model)
+
+Return a `Dict` which can be consumed by the corresponding Stan model.
+"""
+function args_to_stan_data end
+
+"""
+    stan_model_string(model::DynamicPPL.Model)
+
+Return a string defining the Stan model corresponding to `model`.
+"""
+function stan_model_string end
+
+
+const STAN_DEFAULT_COMPILE_FLAGS = [
+    "-ftemplate-depth-256",
+    "-O3",
+    "-mtune=native",
+    "-march=native",
+    "-pipe",
+    "-fno-trapping-math",
+    "-funroll-loops",
+    "-funswitch-loops"
+]
+
+function make_stan_suite(
+    model::DynamicPPL.Model;
+    θ = nothing,
+    model_string = nothing,
+    initial_iters = 10,
+    num_steps = 1,
+    step_size = 1e-3,
+    extra_compile_args = STAN_DEFAULT_COMPILE_FLAGS,
+)
+    if isnothing(model_string)
+        model_string = stan_model_string(model)
+    end
+
+    sm = TuringBenchmarking.pystan.StanModel(
+        model_code = model_string,
+        # NOTE: Extra compile args doesn't seem to make any difference performnace-wise.
+        extra_compile_args = extra_compile_args
+    )
+
+    # Convert the data/observations into something consumable by the Stan model.
+    data = args_to_stan_data(model)
+
+    # Run a tiny bit of sampling because we need the resulting object to compute gradients.
+    f = sm.sampling(
+        data = data,
+        iter = initial_iters,
+        chains = 1,
+        warmup = 0,
+        algorithm = "HMC",
+        control = Dict(
+            "adapt_engaged" => false,
+            # HMC
+            "int_time" => num_steps * step_size,
+            "metric" => "diag_e",
+            "stepsize" => step_size,
+            "stepsize_jitter" => 0,
+        )
+    )
+
+    # Initialize from chain if parameters have not been provided.
+    if isnothing(θ)
+        θ = f.unconstrain_pars(f.get_last_position()[1])
+    end
+
+    # Create suite.
+    stan_suite = BenchmarkGroup()
+    stan_suite["evaluation"] = @benchmarkable $(f.log_prob)($θ)
+    stan_suite["gradient"] = @benchmarkable $(f.grad_log_prob)($θ)
+
+    return stan_suite
+end
+
+end # module TuringBenchmarking
