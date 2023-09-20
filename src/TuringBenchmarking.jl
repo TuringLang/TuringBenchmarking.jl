7718c640923030c3359d16ae6d3c68ba8abb9122
Tor Erlend Fjelde <tor.erlend95@gmail.com>
2023-09-20 11:48:52 +0100

    added pretty printing of checks using PrettyTables.jl

--- src/TuringBenchmarking.jl
+++ src/TuringBenchmarking.jl
@@ -1,9 +1,12 @@
 module TuringBenchmarking
 
+using LinearAlgebra
 using BenchmarkTools
 
 using LogDensityProblems
 using LogDensityProblemsAD
+
+using PrettyTables: PrettyTables
 
 using Turing
 using Turing.Essential: ForwardDiffAD, TrackerAD, ReverseDiffAD, ZygoteAD
@@ -27,7 +30,8 @@ const DEFAULT_ADBACKENDS = [
 ]
 
 backend_label(::ForwardDiffAD) = "ForwardDiff"
-backend_label(::ReverseDiffAD) = "ReverseDiff"
+backend_label(::ReverseDiffAD{false}) = "ReverseDiff"
+backend_label(::ReverseDiffAD{true}) = "ReverseDiff[compiled]"
 backend_label(::ZygoteAD) = "Zygote"
 backend_label(::TrackerAD) = "Tracker"
 
@@ -113,6 +117,8 @@ function make_turing_suite(
     context::DynamicPPL.AbstractContext = DynamicPPL.DefaultContext(),
 )
     grads = Dict(:standard => Dict(), :linked => Dict())
+    grads_and_vals = Dict(:standard => Dict(), :linked => Dict())
+    adbackends = map(to_backend, adbackends)
 
     suite = BenchmarkGroup()
     suite_evaluation = BenchmarkGroup()
@@ -125,7 +131,7 @@ function make_turing_suite(
         context = DynamicPPL.SamplingContext(sampler, context)
     end
 
-    for adbackend in map(to_backend, adbackends)
+    for adbackend in adbackends
         suite_backend = BenchmarkGroup([backend_label(adbackend)])
         suite_gradient["$(adbackend)"] = suite_backend
 
@@ -142,11 +148,11 @@ function make_turing_suite(
         θ = varinfo_current[indexer]
 
         try
-            if run_once
+            if run_once || check_grads
                 ℓ, ∇ℓ = LogDensityProblems.logdensity_and_gradient(f, θ)
 
                 if check_grads
-                    grads[:standard][adbackend] = (ℓ, ∇ℓ)
+                    grads_and_vals[:standard][adbackend] = (ℓ, ∇ℓ)
                 end
             end
             suite_backend["standard"] = @benchmarkable $(LogDensityProblems.logdensity_and_gradient)($f, $θ)
@@ -168,11 +174,11 @@ function make_turing_suite(
         θ_linked = varinfo_linked[indexer]
 
         try
-            if run_once
+            if run_once || check_grads
                 ℓ, ∇ℓ = LogDensityProblems.logdensity_and_gradient(f_linked, θ_linked)
 
                 if check_grads
-                    grads[:linked][adbackend] = (ℓ, ∇ℓ)
+                    grads_and_vals[:linked][adbackend] = (ℓ, ∇ℓ)
                 end
             end
             suite_backend["linked"] = @benchmarkable $(LogDensityProblems.logdensity_and_gradient)($f_linked, $θ_linked)
@@ -195,20 +201,49 @@ function make_turing_suite(
     )
 
     if check_grads
-        # Let's check that the gradients are roughly the same for all backends.
-        (val_first, grad_first) = first(values(grads[:standard]))
-        for (backend, (val, grad)) in grads[:standard]
-            if !(val ≈ val_first)
-                @warn "Gradient check failed for $(backend): log-density values differ"
+        for type in [:standard, :linked]
+            vals = map(first, values(grads_and_vals[type]))
+            vals_dists = compute_distances(adbackends, vals)
+            if !all(isapprox.(values(vals_dists), 0, atol=atol, rtol=rtol))
+                @warn "There is disagreement in the log-density values!"
+                show_distances(vals_dists; header=([titlecase(string(type)), "Log-density"], ["backend", "distance"]), atol=atol, rtol=rtol)
             end
-
-            if !(grad ≈ grad_first)
-                @warn "Gradient check failed for $(backend): gradients differ"
+            grads = map(last, values(grads_and_vals[type]))
+            grads_dists = compute_distances(adbackends, grads)
+            if !all(isapprox.(values(grads_dists), 0, atol=atol, rtol=rtol))
+                @warn "There is disagreement in the gradients!"
+                show_distances(grads_dists, header=([titlecase(string(type)), "Gradient"], ["backend", "distance"]), atol=atol, rtol=rtol)
             end
         end
     end
-    
+
     return suite
+end
+
+function compute_distances(backends, vals)
+    T = eltype(first(vals))
+    n = length(vals)
+    dists = DynamicPPL.OrderedDict{String,T}()
+    for (i, backend_i) in zip(1:n, backends)
+        for (j, backend_j) in zip(i + 1:n, backends[i + 1:end])
+            dists["$(backend_label(backend_i)) vs $(backend_label(backend_j))"] = norm(vals[i] - vals[j])
+        end
+    end
+
+    return dists
+end
+
+function show_distances(dists::AbstractDict; header=["Backend", "Distance"], atol=1e-6, rtol=0)
+    hl = PrettyTables.Highlighter(
+        (data, i, j) -> !isapprox(data[i, 2], 0; atol=atol, rtol=rtol),
+        PrettyTables.crayon"red bold"
+    )
+    PrettyTables.pretty_table(
+        dists;
+        header=header,
+        highlighters=(hl,),
+        formatters=PrettyTables.ft_printf("%.2f", [2])
+    )
 end
 
 """
